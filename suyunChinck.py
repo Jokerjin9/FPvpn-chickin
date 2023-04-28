de0b68cab7d8b53da7f64150df8d5b0fdbbc2e0d
Jokerjin9 <129597814+Jokerjin9@users.noreply.github.com>
2023-04-28 11:11:13 +0800

    Update suyunChinck.py

--- suyunChinck.py
+++ suyunChinck.py
@@ -13,10 +13,10 @@ def suyunChinck():
     try:
         driver = get_web_driver()
         driver.get("https://yooo.one/user")
-        time.sleep(8)
+        time.sleep(12)
         #验证是否为真人
         driver.find_element('xpath', '//*[@id="challenge-stage"]/div/label/input').click()
-        time.sleep(3)
+        time.sleep(8)
         driver.find_element('xpath', '//*[@id="email"]').send_keys(username)
         time.sleep(2)
         driver.find_element('xpath', '//*[@id="password"]').send_keys(password)
