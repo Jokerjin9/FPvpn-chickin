4bea8e9d8051c7fbbfcb26b673729eaf22625d77
Jokerjin9 <129597814+Jokerjin9@users.noreply.github.com>
2023-04-29 09:37:54 +0800

    Update suyunChinck.py

--- suyunChinck.py
+++ suyunChinck.py
@@ -15,8 +15,12 @@ def suyunChinck():
         driver.get("https://yooo.one/user")
         time.sleep(12)
         #验证是否为真人
-        driver.find_element('xpath', '//*[@id="challenge-stage"]/div/label/input').click()
-        time.sleep(8)
+        try:
+            driver.find_element('xpath', '//*[@id="challenge-stage"]/div/label/input').click()
+            time.sleep(6)
+        except NoSuchElementException:
+            print('cf人机验证未找到点击元素')
+        #开始登录
         driver.find_element('xpath', '//*[@id="email"]').send_keys(username)
         time.sleep(2)
         driver.find_element('xpath', '//*[@id="password"]').send_keys(password)
@@ -28,9 +32,8 @@ def suyunChinck():
             driver.find_element('xpath', '//*[@id="checkin"]').click()
             time.sleep(2)
         except NoSuchElementException:
-            pass
+            print('签到元素不可用')
         finally: 
-            print('按钮不可点击，已签到？？')
             #获取元素值
             result1 = driver.find_element('xpath', '//*[@id="kt_subheader"]/div/div[2]/a').text
             result = '签到状态：' + result1
@@ -40,18 +43,18 @@ def suyunChinck():
             left = '流量剩余：' + left1
            
     except:
-        pass
+        print('整个过程失败')
     finally:
         driver.quit()
-    time.sleep(3)
+    time.sleep(1)
     topic = '速云签到'
    
     #发送邮件
-    ret = mail(topic,result,left,my_sender,my_user,my_pass)
-    if ret:
-        print("邮件发送成功")
-    else:
-        print("邮件发送失败")
-    return result
+#     ret = mail(topic,result,left,my_sender,my_user,my_pass)
+#     if ret:
+#         print("邮件发送成功")
+#     else:
+#         print("邮件发送失败")
+    return 
 if __name__ == '__main__':
     suyunChinck()
